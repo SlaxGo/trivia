428c36b191e74a7c71a0ab278fae0e76b4b8cbee
slaxgo <salvatore.marino01@hotmail.it>
2024-02-19 11:47:28 +0100

    agg home

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -2,13 +2,22 @@ function home() {
     return (
         <div className="bg-primary vh-100 d-flex align-items-center">
             <div className="container">
-                <div className="row">
-                    <div className="row">
-                        <div className="col-12">
-                        <div class="form-floating mb-3">
-                            <input type="text" class="form-control" id="floatingInput" placeholder=""/>
-                                <label for="floatingInput">NickName</label>
-                        </div>
+                <div className="row justify-content-center">
+                    <div className="col-6 bg-secondary rounded-4">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-12">
+                                    <div class="form-floating">
+                                        <input type="text" class="form-control" id="floatingInput" placeholder="" />
+                                        <label for="floatingInput">NickName</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-12">
+                                <button type="button" class="btn btn-primary">Primary</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
