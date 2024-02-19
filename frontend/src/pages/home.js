cd5fc96d46689cfe5bfadf9e010120d7d1716f0d
slaxgo <salvatore.marino01@hotmail.it>
2024-02-19 12:08:14 +0100

    button and react smooth corner

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,21 +1,24 @@
+import Logo from '../images/trivia.png'
+
 function home() {
     return (
         <div className="bg-primary vh-100 d-flex align-items-center">
             <div className="container">
                 <div className="row justify-content-center">
-                    <div className="col-6 bg-secondary rounded-4">
-                        <div className="container">
+                    <div className="col-12 col-md-6">
+                        <div className="container bg-secondary rounded-3 pt-5 pb-4 position-relative">
+                            <img src={Logo} className="position-absolute top-0 start-50 translate-middle w-50" alt='Trivia Logo'></img>
                             <div className="row">
                                 <div className="col-12">
-                                    <div class="form-floating">
+                                    <div class="form-floating my-3">
                                         <input type="text" class="form-control" id="floatingInput" placeholder="" />
                                         <label for="floatingInput">NickName</label>
                                     </div>
                                 </div>
                             </div>
                             <div className="row">
-                                <div className="col-12">
-                                <button type="button" class="btn btn-primary">Primary</button>
+                                <div className="col-12 text-center">
+                                    <button type="button" class="btn btn-secondary fw-bold">Play ▶</button>
                                 </div>
                             </div>
                         </div>
