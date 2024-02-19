7c171e688273c746bb9c86a1ca42960aafac36fe
slaxgo <salvatore.marino01@hotmail.it>
2024-02-19 11:23:55 +0100

    agg home

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,8 +1,19 @@
-function home(){
-    return(
-    <div className="bg-secondary">
-        Ciao
-    </div>
+function home() {
+    return (
+        <div className="bg-primary vh-100 d-flex align-items-center">
+            <div className="container">
+                <div className="row">
+                    <div className="row">
+                        <div className="col-12">
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="floatingInput" placeholder=""/>
+                                <label for="floatingInput">NickName</label>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
